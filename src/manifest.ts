a511f0b8ced07b7f8a85563721cc72c9a9143681
ReedSun <superreedsun@gmail.com>
2021-11-27 16:35:19 +0800

    feat: change directory structure

--- src/manifest.ts
+++ src/manifest.ts
@@ -4,7 +4,7 @@
  * File Created: Thursday, 2021-11-25 17:08:26
  * Author: ReedSun (reedsun@shanjing-inc.com)
  * -----
- * Last Modified: Thursday, 2021-11-25 17:43:52
+ * Last Modified: Saturday, 2021-11-27 16:33:04
  * Modified By: ReedSun (reedsun@shanjing-inc.com)
  * -----
  * Copyright 2013 - 2021, Qingdao Shanjing Information Technology Co., Ltd.
@@ -14,11 +14,11 @@ import { ManifestV3 } from 'rollup-plugin-chrome-extension';
 const manifest: ManifestV3 = {
   action: { default_popup: 'pages/popup/index.html' },
   background: {
-    service_worker: 'background/index.ts',
+    service_worker: 'service_worker.ts',
   },
   content_scripts: [
     {
-      js: ['content/index.ts'],
+      js: ['content_scripts.ts'],
       matches: ['https://*.example.com/*'],
     },
   ],
