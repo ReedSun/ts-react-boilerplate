37ace4d1adf7a54dfb27f274bebd7e67c08eae3a
ReedSun <superreedsun@gmail.com>
2021-12-11 18:03:36 +0800

    feat: use @reedsun/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    webextensions: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint', 'typescript-sort-keys'],
-  rules: {
-    'max-lines': [2, { skipBlankLines: true, skipComments: true }],
-    'max-lines-per-function': 2,
-    'no-alert': 1,
-    'no-console': 1,
-    'no-debugger': 1,
-    'no-else-return': 2,
-    'object-shorthand': 2,
-    'prefer-template': 2,
-    'sort-imports': 2,
-    'sort-keys': 2,
-    'typescript-sort-keys/interface': 2,
-    'typescript-sort-keys/string-enum': 2,
-  },
+  extends: ['@reedsun'],
 };
